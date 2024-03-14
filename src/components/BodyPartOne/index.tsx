f7e87b65c07a5201d77d5e740da5e740e8d1101f
gyanendra19 <gyanendrav416@gmail.com>
2024-03-14 20:41:05 +0530

    Added comments

--- src/components/BodyPartOne/index.tsx
+++ src/components/BodyPartOne/index.tsx
@@ -4,7 +4,9 @@ import { RiMapPinLine, RiCalendarLine, RiShieldLine} from '@remixicon/react'
 type Props = {}
 
 function BodyPartOne({ }: Props) {
-    const locationMarkup = (logo: any, head: String, text: String) => {
+
+    // Function for the info of the vaccines
+    const vaccineInfo = (logo: any, head: String, text: String) => {
         return <div className='flex gap-2 ml-8'>
         <div>
             <div className='w-10 h-10 mt-2 rounded-md bg-[#C4C4C4] opacity-10 flex justify-center items-center'>
@@ -22,13 +24,20 @@ function BodyPartOne({ }: Props) {
         </div>
     </div>
     }
+
+
     return (
         <section className='w-full h-fit relative'>
+
+            {/* BACKGROUND IMAGE */}
             <div className='before:absolute before:bg-[#0B153C] before:z-[-1] before:h-full before:w-full before:opacity-80'></div>
             <div className='w-full md:h-[900px] h-[1008px] absolute z-[-10]'>
                 <img className='h-full' src='/vaccine-bg.jpeg' alt="" />
             </div>
 
+            
+
+            {/* VACCINE HEAD */}
             <div className='py-14 w-full'>
                 <div className='w-[90%] mx-auto'>
                     <div className='md:flex justify-between'>
@@ -37,16 +46,15 @@ function BodyPartOne({ }: Props) {
                                 <p className='md:text-5xl text-2xl font-extrabold text-white'>COVID-19 Vaccination Got Easier With,</p>
                                 <span className='text-[#17C2EC] font-extrabold md:text-5xl text-2xl'>Vaccination.ng</span>
 
-                                <p className='md:mt-12 mt-6 opacity-70 text-white md:leading-7 md:text-2xl text-md font-medium'>Vaccination.ng will help you find the nearest
+                                <p className='md:mt-12 mt-6 opacity-70 text-white md:leading-7 md:text-2xl font-medium'>Vaccination.ng will help you find the nearest
                                     centre for vaccination, in all 36 states in Nigeria.</p>
 
                                 <div className="flex md:mt-12 mt-8 gap-6 ml-6 md:ml-0">
-                                    <button className='whitespace-nowrap px-4 md:py-2 py-3 md:text-xl text-sm bg-[#17C2EC] text-white font-semibold rounded-[16px]'>Get Vaccine</button>
-                                    <button className='px-5 whitespace-nowrap md:text-xl py-3 bg-[#2e2c4d] border-2 border-[#17C2EC] text-white font-semibold text-sm rounded-[16px] '>Help Centre</button>
+                                    <button className='px-4 md:py-2 py-3 md:text-xl text-sm bg-[#17C2EC] text-white font-semibold rounded-[16px]'>Get Vaccine</button>
+                                    <button className='px-5 md:text-xl py-3 bg-[#2e2c4d] border-2 border-[#17C2EC] text-white font-semibold text-sm rounded-[16px] '>Help Centre</button>
                                 </div>
                             </div>
                         </div>
-
 
                         <div className='mt-10 md:mt-0 ml-12 md:ml-0'>
                             <img className='h-[250px] md:h-full' src="/vaccine.png" alt="" />
@@ -54,13 +62,15 @@ function BodyPartOne({ }: Props) {
                     </div>
 
 
+
+                    {/* VACCINE INFO */}
                     <div className='mt-16 w-full md:h-[160px] h-[230px] bg-[#C4C4C4] md:opacity-5 opacity-10 relative here rounded-tl-2xl rounded-tr-2xl'>
                     </div>
                     <div className='flex relative'>
                         <div className='flex flex-col md:flex-row gap-2 md:gap-0 justify-evenly md:w-full h-full absolute md:-top-28 -top-[118px]'>
-                            {locationMarkup(<RiMapPinLine color='#17C2EC' />, 'Location', 'Ikeja Lagos, Nigeria')}
-                            {locationMarkup(<RiCalendarLine color='#17C2EC' />, 'Date', '29 December, 2022')}
-                            {locationMarkup(<RiShieldLine color='#17C2EC' />, 'Vaccine Type', 'Mordena')}
+                            {vaccineInfo(<RiMapPinLine color='#17C2EC' />, 'Location', 'Ikeja Lagos, Nigeria')}
+                            {vaccineInfo(<RiCalendarLine color='#17C2EC' />, 'Date', '29 December, 2022')}
+                            {vaccineInfo(<RiShieldLine color='#17C2EC' />, 'Vaccine Type', 'Mordena')}
                          <button className='md:ml-40 md:h-[50px] p-3 md:text-xl bg-[#17C2EC] text-white font-semibold rounded-[12px] w-28 ml-8 h-[60px]'>Submit</button>
                         </div>
                     </div>
--- src/components/BodyPartTwo/index.tsx
+++ src/components/BodyPartTwo/index.tsx
@@ -4,6 +4,8 @@ type Props = {}
 function BodyPartTwo({ }: Props) {
   return (
     <section className='w-full h-fit relative'>
+
+      {/* BACKGROUND IMAGE */}
       <div className='before:absolute before:bg-[#0B153C] before:z-[-1] before:h-full before:w-full before:opacity-80'></div>
       <div className='w-full h-[390px] absolute z-[-10]'>
         <img className='h-full w-full' src='/vaccine-bg.jpeg' alt="" />
@@ -11,22 +13,29 @@ function BodyPartTwo({ }: Props) {
 
       <div className='py-16 w-full'>
         <div className='w-[90%] mx-auto'>
-          <div className='w-full h-[265px] relative bg-[#C4C4C4] opacity-5 rounded-tr-[20px] rounded-tl-[20px]'>
+
+
+      {/* RESULTS INPUT */}
+          <div className='w-full h-[265px] relative bg-[#C4C4C4] opacity-5 rounded-tr-[20px] rounded-tl-[20px]'></div>
+
+          <div className='absolute md:top-[25%] top-[19%] text-white md:ml-10 ml-4 font-semibold md:text-xl text-[12px]'>
+            <p>Check your COVID-19 result on our Database</p>
           </div>
-        <div className='absolute md:top-[25%] top-[19%] text-white md:ml-10 ml-4 font-semibold md:text-xl text-[12px]'>
-          <p>Check your COVID-19 result on our Database</p>
-        </div>
-        <div className='absolute md:top-[55%] top-[30%] text-white md:ml-10 ml-6 font-semibold md:text-xl flex flex-col md:flex-row ms:gap-8 gap-4 md:items-center'>
-          <div className='md:w-[450px] w-[260px] md:p-4 p-3 md:h-[70px] h-[50px] border-2 border-[#17C2EC] rounded-2xl'>
-          Okeowo
+
+          <div className='absolute md:top-[55%] top-[30%] text-white md:ml-10 ml-6 font-semibold md:text-xl flex flex-col md:flex-row ms:gap-8 gap-4 md:items-center'>
+            <div className='md:w-[450px] w-[260px] md:p-4 p-3 md:h-[70px] h-[50px] border-2 border-[#17C2EC] rounded-2xl'>
+              Okeowo
+            </div>
+
+            <div className='md:w-[450px] w-[260px] opacity-40 md:p-4 p-3 md:h-[70px] h-[50px] border-2 border-[#C4C4C4] rounded-2xl'>
+              NIK Number
+            </div>
+
+            <button className='md:ml-16 md:h-[50px] h-[55px] md:text-xl bg-[#17C2EC] text-white font-semibold rounded-[12px] w-[100px]'>Check</button>
           </div>
-          <div className='md:w-[450px] w-[260px] opacity-40 md:p-4 p-3 md:h-[70px] h-[50px] border-2 border-[#C4C4C4] rounded-2xl'>
-          NIK Number
-          </div>
-          <button className='md:ml-16 md:h-[50px] h-[55px] md:text-xl bg-[#17C2EC] text-white font-semibold rounded-[12px] w-[100px]'>Check</button>
-        </div>
+
           <div className='here-again relative'></div>
-        </div>
+          </div>
 
       </div>
     </section>
